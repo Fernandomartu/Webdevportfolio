c7c0a8c30aa62e4f72c61fa4a8309fabeac0b113
Fernando Marturet <fernando.martu@gmail.com>
2022-06-14 10:45:57 -0400

    adding new sound file

--- scripts/linesanimations.js
+++ scripts/linesanimations.js
@@ -2,12 +2,14 @@
 let nameWrap = document.getElementById("name-wrap");
 let descriptionParagraph = document.getElementById("description-paragraph-wrap");
 let leftColumnWrap = document.getElementById("left-column-wrap");
-
+let song = new Audio('./sound files/fightagainst.m4a');
 
 let moveNameWrap = () => {
+    song.play();
     leftColumnWrap.style.left = '150px';
     nameWrap.style.fontSize = "50px";
     descriptionParagraph.style.display = "inline";
+
 }
 
 moveNameWrap();
