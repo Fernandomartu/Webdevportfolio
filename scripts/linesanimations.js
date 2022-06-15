faff298ed2469e97538585366be0d7c4e4291a0f
Fernando Marturet <fernando.martu@gmail.com>
2022-06-15 12:37:23 -0400

    not sure just pushing

--- scripts/linesanimations.js
+++ scripts/linesanimations.js
@@ -1,6 +1,6 @@
 
 let nameWrap = document.getElementById("name-wrap");
-let descriptionParagraph = document.getElementById("description-paragraph-wrap");
+let descriptionParagraph = document.getElementById("description-paragraph");
 let mainBodyWrap = document.getElementById("main-body-wrap");
 let nameDescWrap = document.getElementById("name-desc-wrap");
 let testWrap = document.getElementById("test-wrap");
@@ -10,7 +10,15 @@ let moveNameWrap = () => {
     song.play();
     nameDescWrap.style.width = "50%";
     nameWrap.style.fontSize = "50px";
-
+    descriptionParagraph.style.display = "inline-block";
 }
+
+let loadNextAnim = () => {
+    const script = document.createElement('script');
+    script.src = './scripts/scrambleTextAnimation.js';
+    document.head.prepend(script);
+    }
+
+setTimeout(loadNextAnim, 5000);
 
 moveNameWrap();
