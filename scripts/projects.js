9d4debce331f12f89a39f1f7adefd2a4356aff13
Excela La Rosa <excelalarosa@Excelas-MBP.attlocal.net>
2022-06-24 10:21:21 -0400

    added projects file

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,15 +1,17 @@
-function Project(title, image, description, technologies, projectID) {
+function Project(title, image, description, technologies, projectID, imgID) {
     this.title = title;
     this.image = image;
     this.description = description;
     this.technologies = technologies;
     this.appendData = function(){
         let project = document.getElementById(projectID);
-        let img = document.createElement('img');
+        let img = document.getElementById(imgID)
         img.src = this.image;
-        pr
+        console.log(img);
     }
     this.appendData();
   }
 
-  let projectOne = new Project("Title Test", "./images/6.webp", "testing a short description", "html, css, javascript", "project-one");
+  let projectOne = new Project("Title Test", "images/6.webp", "testing a short description", "html, css, javascript", "project-one", "project-one-image");
+
+  
