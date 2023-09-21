9a1e621809c1e1a586fe4827cb44cdf6c9ca72c2
Fernando Marturet <fernando.martu@gmail.com>
2023-09-20 22:46:55 -0400

    fixing nav issue with github button

--- scripts/projectNavigation.js
+++ scripts/projectNavigation.js
@@ -1,9 +1,13 @@
+let gitButtonOne = document.getElementById("git-button-one");
+
+let isGitHovered = false;
+
 let projects = document.getElementsByClassName("project");
 
 projects[0].onclick = function () {
-  if (window.innerWidth <= 800) {
+  if (window.innerWidth <= 800 && !isGitHovered) {
     window.open(
-      "https://github.com/Fernandomartu/MERN-portfolio-project",
+      "https://mern-social-media-frontend-15rx.onrender.com/",
       "_blank"
     );
   } else return;
@@ -20,3 +24,13 @@ projects[2].onclick = function () {
     window.open("https://epicprint.com", "_blank");
   } else return;
 };
+
+gitButtonOne.addEventListener("mouseover", () => {
+  isGitHovered = true; // Change the background color to blue on hover
+});
+
+// Add a "mouseout" event listener to revert the changes when the mouse leaves the button
+gitButtonOne.addEventListener("mouseout", () => {
+  // Code to execute when the mouse leaves the button
+  isGitHovered = false; // Revert the background color to its original state
+});
